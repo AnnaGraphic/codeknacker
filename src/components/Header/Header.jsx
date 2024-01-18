77cd912c789aa977cf122eac8a9e911671c24bf2
panda <p4nd4@posteo.de>
2024-01-18 22:24:56 +0100

    add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+export function Header () {
+  return (
+    <header className="header">
+    <div className="header_logo">Logo</div>
+    <div className="header_title">Codeknacker</div>
+  </header>
+  )
+}
