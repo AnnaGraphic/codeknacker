c4216e06208de9f9a2d2b88b0218861d6aa32510
panda <p4nd4@posteo.de>
2024-01-18 22:24:21 +0100

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import './App.css'
+import{ useState } from 'react';
+import { createBrowserRouter, RouterProvider, Navigate, Routes, Route } from "react-router-dom";
+import { Layout } from './components/Layout/Layout.jsx';
+import { Start } from './components/TESTS/Start.jsx';
+import { Login } from './components/TESTS/Login.jsx';
 
 function App() {
 
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Start />,
+        },
+        {
+          path: 'login',
+          element: isLoggedIn ? <Navigate to="/" /> : <Login/>,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <>
-      <p>hello world</p>
-    </>
+      <RouterProvider router={router} fallbackElement={<p>loading..</p>} />
   )
 }
 
