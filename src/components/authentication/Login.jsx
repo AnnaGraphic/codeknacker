44b44e0b744f20a33e27b147371ef8c7067a69b8
emanuela <emanuela.markovic@hotmail.com>
2024-01-22 10:47:54 +0100

    Update onSubmit function

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 const Login = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <div>
-        <form className="form">
-          <p>please Login</p>
+        <form className="form" onSubmit={onSubmit}>
+          <p>Please Login !</p>
           <input type="text" placeholder="username" />
           <input
             type="password"
             placeholder="password"
             autoComplete="new-password"
           />
-          <button>Log In</button>
+          <button className="submit" type="submit">
+            Log In
+          </button>
         </form>
       </div>
     </div>
