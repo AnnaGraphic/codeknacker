1fd46d03fb9a69fbee6e3e56669734a445df6d3f
emanuela <emanuela.markovic@hotmail.com>
2024-01-22 10:18:03 +0100

    Format code in Login component

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="App">
-    <div>
-    <form className="form" >
-      <p>please Login</p>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" autoComplete="new-password"/>
-      <button>Log In</button>
-    </form>
+  return (
+    <div className="App">
+      <div>
+        <form className="form">
+          <p>please Login</p>
+          <input type="text" placeholder="username" />
+          <input
+            type="password"
+            placeholder="password"
+            autoComplete="new-password"
+          />
+          <button>Log In</button>
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
