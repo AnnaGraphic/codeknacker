f077224387f9c723d735045b3ae55f923abef027
Miriam <miriam.bonninghoff@dci-student.org>
2024-01-22 11:50:43 +0100

    conflict app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,36 @@ import {
 import { Layout } from "./components/Layout/Layout.jsx";
 import { Start } from "./components/TESTS/Start.jsx";
 import { Login } from "./components/TESTS/Login.jsx";
-import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { Layout } from "./components/Layout/Layout.jsx";
-import { Start } from "./components/TESTS/Start.jsx";
-import { Login } from "./components/TESTS/Login.jsx";
 
 function App() {
-  return (
-    <>
-      <p>hello world</p>
-    </>
-  );
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Start />,
+        },
+        {
+          path: "login",
+          element: isLoggedIn ? <Navigate to="/" /> : <Login />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} fallbackElement={<p>loading..</p>} />;
 }
 
 export default App;
