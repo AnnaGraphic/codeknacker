e87bae3cd2ce626464e9d410b304d15bce263995
emanuela <emanuela.markovic@hotmail.com>
2024-01-24 12:03:53 +0100

    branch erstellt start und die componente Start erstellt und in die App.jsx importiert

---
+++ src/components/Start.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CodeknackerSVG from "../assets/codeknacker.svg";
+
+const Start = () => {
+  return (
+    <>
+      <h1>CODE</h1>
+      <h3>cnacker</h3>
+      <img src={CodeknackerSVG} alt="Codecnacker" />
+    </>
+  );
+};
+
+export default Start;
