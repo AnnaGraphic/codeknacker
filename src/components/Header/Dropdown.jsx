f3eef2efab638017d884d9827eca9737b1527e3d
emanuela <emanuela.markovic@hotmail.com>
2024-01-24 16:03:05 +0100

    start.css style added to start componente and burger svg inserted, import start componente into app.jsx

--- src/components/Header/Dropdown.jsx
+++ src/components/Header/Dropdown.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
-import { dropdownLinks } from '../../data/dropdown.js'
+import { Link } from "react-router-dom";
+import { dropdownLinks } from "../../data/dropdown.js";
+import BurgerSvg from "../../assets/burger.svg";
 
 export function Dropdown({ open, handleOpen }) {
   return (
     <div className="dropdown">
       <button onClick={handleOpen}>
-        X
+        <img src={BurgerSvg} alt="burger" />
       </button>
       {open && (
         <div>
