ff6ab671b53a61fd37bb449a819110fccd3d35a4
emanuela <emanuela.markovic@hotmail.com>
2024-01-26 15:27:03 +0100

    made main responsive and changed the name in login

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -95,7 +95,7 @@ const Login = () => {
           <form className="form" onSubmit={onSubmit}>
             <h1>Sign in to your account</h1>
             {error && error.general && <p className="error">{error.general}</p>}
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">Username*</label>
             <input
               type="text"
               placeholder=""
@@ -109,7 +109,7 @@ const Login = () => {
                 })
               }
             />
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password*</label>
             <input
               type="password"
               placeholder=""
